c3ed08bde6c597262dae4fbd7651a54dea872ede
sisyphys_999 <139012668+Umer-Islam@users.noreply.github.com>
2024-09-20 17:05:47 +0500

    numbers_exercise

--- ruby_basics/1_data_types/exercises/numbers_exercises.rb
+++ ruby_basics/1_data_types/exercises/numbers_exercises.rb
@@ -4,23 +4,29 @@ def add(a, b)
 end
 
 def subtract(a, b)
+
   # return the result of subtracting b from a
+  a-b
 end
 
 def multiply(a, b)
   # return the result of multiplying a times b
+  a*b
 end
 
 def divide(a, b)
   # return the result of dividing a by b
+  a/b
 end
 
 def remainder(a, b)
   # return the remainder of dividing a by b using the modulo operator
+  remainer(a,b)
 end
 
 def float_division(a, b)
   # return the result of dividing a by b as a float, rather than an integer
+  a.to_f/b
 end
 
 def string_to_number(string)
